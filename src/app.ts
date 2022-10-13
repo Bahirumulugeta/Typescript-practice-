b25c588d0591fc6087fb06724d148b2dc31f5c75
bahiru <bahirumulugeta1@gmail.com>
2022-10-13 19:12:36 +0200

    inheritance with typescript

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,46 @@
-class Department{
-    name : string;
-    constructor(n:string){
-        this.name = n;
-    }
-  
-  
+class Department {
+  private employees:string[]=[];
+  constructor(private readonly id:string,public name:string) {
+  }
+  describe(this:Department) {
+    console.log(`Department (${this.id}):${this.name}`);
+  }
+  addEmployee(employee:string){
+    this.employees.push(employee);
+  }
+  printEmployeeInformation(){
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
-const obj = new Department("Accounting");
-  console.log(obj);
+class ITDepartment extends Department {
+  admins:string[];
+  constructor(id: string, admins: string[]) {
+    super(id,"IT");
+    this.admins=admins;
+  }
+}
+class Accounting extends Department{
+  constructor(id:string,private reports:string[]){
+    super(id,'Accounting');
+  }
+  addReport(text:string){
+   this.reports.push(text);
+  }
+  printReport(){
+    console.log(this.reports);
+  }
+}
+const it = new ITDepartment('d1',['Bahir']);
+it.addEmployee("Bahiru");
+it.addEmployee("Mulugeta");
+it.describe();
+it.name="New Name";
+it.printEmployeeInformation();
+console.log(it);
+
+const  accounting = new Accounting('d2',[]);
+accounting.addReport('something went wrong...');
+accounting.printReport();
+
+
