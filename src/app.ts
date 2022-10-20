3653efdffe1189417f5dac10803548e6e2bd5725
bahiru <bahirumulugeta1@gmail.com>
2022-10-20 14:50:39 +0200

    adding generic function

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,5 @@
-interface Named{
-  readonly name: string;
-
+function merge<T extends object, U extends object>(name:T, objB:U){
+  return Object.assign(name,objB);
 }
-interface Greetable extends Named{
-  greet(phrase: string): void;
-}
-class Person implements Greetable {
-  name: string;
-  age=14;
-  constructor(n: string) {
-    this.name = n;
-  }
-  greet(phrase:string) {
-    console.log(phrase + " " + this.name);
-
-  }
-}
-let user1: Greetable;
-user1 = new Person("Bahiru");
-user1.greet("Heyy man");
-console.log(user1);
+const newObject=merge({name:'Bahiru'},{id:12});
+console.log(newObject.name);
