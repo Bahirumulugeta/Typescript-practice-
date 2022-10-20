190e42f597f9aeda7f06ea44572ac7e6b5d8ea05
bahiru <bahirumulugeta1@gmail.com>
2022-10-20 16:04:46 +0200

    usage of partial generic utility

--- src/app.ts
+++ src/app.ts
@@ -1,43 +1,57 @@
-function merge<T extends object, U extends object>(name: T, objB: U) {
-  return Object.assign(name, objB);
+// function merge<T extends object, U extends object>(name: T, objB: U) {
+//   return Object.assign(name, objB);
+// }
+// const newObject = merge({ name: "Bahiru" }, { id: 12 });
+//  console.log(newObject);
+
+// function extractAndConvert<T extends object, U extends keyof T>(
+//   obj: T,
+//   key: U
+// ) {
+//   return obj[key];
+// }
+// extractAndConvert({ name: "Bahiru" }, "name");
+
+// class DataStorage<T> {
+//   private data: T[] = [];
+//   addItem(item: T) {
+//     this.data.push(item);
+//   }
+//   removeItem(item: T) {
+//     if (this.data.indexOf(item) === -1) return;
+//     this.data.splice(this.data.indexOf(item), 1);
+//   }
+//   getItems() {
+//     return [...this.data];
+//   }
+// }
+// const textStorage = new DataStorage<string>();
+// textStorage.addItem("Bahiru");
+// textStorage.addItem("Mulugeta");
+// textStorage.removeItem("Bahiru");
+// console.log(textStorage.getItems());
+
+// const numberStorage = new DataStorage<number>();
+// numberStorage.addItem(1);
+// console.log(numberStorage.getItems());
+
+// const objectStorage = new DataStorage<object>();
+// objectStorage.addItem({ name: "Bahiru" });
+// objectStorage.addItem({ id: 1 });
+
+// console.log(objectStorage.getItems());
+
+interface CourseGoal{
+  title:string,
+  description:string,
+  completeUntil:Date
 }
-const newObject = merge({ name: "Bahiru" }, { id: 12 });
-// console.log(newObject);
+function createCourseGoal (title:string,description:string,date:Date):CourseGoal{
+  let courseGoal: Partial<CourseGoal> ={};
+  courseGoal.title= title;
+  courseGoal.description= description;
+  courseGoal.completeUntil= date;
 
-//keyof usage
-function extractAndConvert<T extends object, U extends keyof T>(
-  obj: T,
-  key: U
-) {
-  return obj[key];
+  return courseGoal as CourseGoal;
 }
-extractAndConvert({ name: "Bahiru" }, "name");
 
-class DataStorage<T> {
-  private data: T[] = [];
-  addItem(item: T) {
-    this.data.push(item);
-  }
-  removeItem(item: T) {
-    if (this.data.indexOf(item) === -1) return;
-    this.data.splice(this.data.indexOf(item), 1);
-  }
-  getItems() {
-    return [...this.data];
-  }
-}
-const textStorage = new DataStorage<string>();
-textStorage.addItem("Bahiru");
-textStorage.addItem("Mulugeta");
-textStorage.removeItem("Bahiru");
-console.log(textStorage.getItems());
-
-const numberStorage = new DataStorage<number>();
-numberStorage.addItem(1);
-console.log(numberStorage.getItems());
-
-const objectStorage = new DataStorage<object>();
-objectStorage.addItem({ name: "Bahiru" });
-objectStorage.addItem({ id: 1 });
-
-console.log(objectStorage.getItems());
