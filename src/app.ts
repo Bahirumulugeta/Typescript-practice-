a9cc996d4258970ee017c7f51b032981c4d5e078
bahiru <bahirumulugeta1@gmail.com>
2022-10-20 15:23:01 +0200

    generic class

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,43 @@
-function merge<T extends object, U extends object>(name:T, objB:U){
-  return Object.assign(name,objB);
+function merge<T extends object, U extends object>(name: T, objB: U) {
+  return Object.assign(name, objB);
 }
-const newObject=merge({name:'Bahiru'},{id:12});
-console.log(newObject.name);
+const newObject = merge({ name: "Bahiru" }, { id: 12 });
+// console.log(newObject);
 
 //keyof usage
-function extractAndConvert<T extends object,U extends keyof T>(obj:T,key:U){
-  return obj[key]; 
+function extractAndConvert<T extends object, U extends keyof T>(
+  obj: T,
+  key: U
+) {
+  return obj[key];
 }
-extractAndConvert({name:'Bahiru'},'name');
+extractAndConvert({ name: "Bahiru" }, "name");
+
+class DataStorage<T> {
+  private data: T[] = [];
+  addItem(item: T) {
+    this.data.push(item);
+  }
+  removeItem(item: T) {
+    if (this.data.indexOf(item) === -1) return;
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+  getItems() {
+    return [...this.data];
+  }
+}
+const textStorage = new DataStorage<string>();
+textStorage.addItem("Bahiru");
+textStorage.addItem("Mulugeta");
+textStorage.removeItem("Bahiru");
+console.log(textStorage.getItems());
+
+const numberStorage = new DataStorage<number>();
+numberStorage.addItem(1);
+console.log(numberStorage.getItems());
+
+const objectStorage = new DataStorage<object>();
+objectStorage.addItem({ name: "Bahiru" });
+objectStorage.addItem({ id: 1 });
+
+console.log(objectStorage.getItems());
