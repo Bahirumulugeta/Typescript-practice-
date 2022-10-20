b3d1a27a2c4fdf81934561f0fee34c7f19aa84e7
bahiru <bahirumulugeta1@gmail.com>
2022-10-20 15:02:58 +0200

    keyof

--- src/app.ts
+++ src/app.ts
@@ -3,3 +3,9 @@ function merge<T extends object, U extends object>(name:T, objB:U){
 }
 const newObject=merge({name:'Bahiru'},{id:12});
 console.log(newObject.name);
+
+//keyof usage
+function extractAndConvert<T extends object,U extends keyof T>(obj:T,key:U){
+  return obj[key]; 
+}
+extractAndConvert({name:'Bahiru'},'name');
