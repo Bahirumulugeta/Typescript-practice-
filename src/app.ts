d20212d74fcf11ae19880079113fdcf938d6340f
bahiru <bahirumulugeta1@gmail.com>
2022-10-21 14:27:08 +0200

    decorator factories

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
-function Logger(constructor:Function){
-  console.log("logging ...");
-  console.log(constructor);
+function Logger(logString:string){
+  return function(constructor:Function){
+    console.log(logString);
+    console.log(constructor);
+  }
 }
-@Logger
+@Logger('LOGGING-PERSON')
 class Person{
   name="Bahiru";
   constructor(){
